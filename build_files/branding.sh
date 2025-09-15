4c2034a3f02812d233bdf2724f9c6f7efda6b54c
Cameron Pittman <cameron.w.pittman@gmail.com>
2025-09-15 11:10:50 -0400

    improve handling version strings

--- build_files/branding.sh
+++ build_files/branding.sh
@@ -2,6 +2,12 @@
 
 set ${SET_X:+-x} -eou pipefail
 
+# VERSION should just be the date, but it might not have been set
+version_date=${VERSION}
+if [ -z "${VERSION}" ]; then
+  version_date=$(date +%Y%m%d)
+fi
+
 tee /usr/share/ublue-os/image-info.json <<'EOF'
 {
   "image-name": "",
@@ -18,16 +24,16 @@ cat <<<"$(jq ".\"image-name\" |= \"orcus\" |
               .\"image-flavor\" |= \"kinoite-main\" |
               .\"image-vendor\" |= \"cameronwp\" |
               .\"image-ref\" |= \"ostree-image-signed:docker://ghcr.io/cameronwp/orcus\" |
-              .\"image-tag\" |= \"${IMAGE}${VERSION}\" |
+              .\"image-tag\" |= \"${IMAGE}${version_date}\" |
               .\"base-image-name\" |= \"${BASE_IMAGE}\" |
               .\"fedora-version\" |= \"$(rpm -E %fedora)\"" \
     </usr/share/ublue-os/image-info.json)" \
 >/tmp/image-info.json
 cp /tmp/image-info.json /usr/share/ublue-os/image-info.json
 
-sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"$(echo "${IMAGE^}" | cut -d - -f1) (Version: ${VERSION} / FROM ${BASE_IMAGE^} $(rpm -E %fedora))\"|" /usr/lib/os-release
-sed -i "s|^VERSION=.*|VERSION=\"${VERSION} (${BASE_IMAGE^})\"|" /usr/lib/os-release
+sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"$(echo "${IMAGE^}" | cut -d - -f1) (Version: ${version_date} / FROM ${BASE_IMAGE^} $(rpm -E %fedora))\"|" /usr/lib/os-release
+sed -i "s|^VERSION=.*|VERSION=\"${version_date} (${BASE_IMAGE^})\"|" /usr/lib/os-release
 sed -i "s|^OSTREE_VERSION=.*|OSTREE_VERSION=\'${VERSION}\'|" /usr/lib/os-release
 echo "HOME_URL=\"https://github.com/cameronwp/${IMAGE}\"" >>/usr/lib/os-release
 echo "IMAGE_ID=\"${IMAGE}\"" >>/usr/lib/os-release
-echo "IMAGE_VERSION=\"${VERSION}\"" >>/usr/lib/os-release
+echo "IMAGE_VERSION=\"${version_date}\"" >>/usr/lib/os-release
