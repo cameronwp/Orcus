e6e62151eb4b3a5c64b5c1ce03536f86bfaa97c2
Cameron Pittman <cameron.w.pittman@gmail.com>
2025-09-19 15:45:50 -0400

    fix handling global install of doom-updater; update doom updater logic to include backup cleaning

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,13 +11,14 @@ set -ouex pipefail
 
 
 ###
-### manage root packages
+### manage root-level packages
 ###
 
-# installs packages from fedora repos
+# packages from fedora repos
 dnf5 install -y $(</ctx/packages.txt)
 
 # install VS Code
+# https://code.visualstudio.com/docs/setup/linux#_rhel-fedora-and-centos-based-distributions
 rpm --import https://packages.microsoft.com/keys/microsoft.asc && \
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf5 install -y code
@@ -27,11 +28,11 @@ dnf5 install -y code
 sudo dnf5 config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 sudo dnf5 install -y tailscale
 
-# remove the firefox RPM that's missing codecs
-dnf5 remove -y firefox
-
 # VMs
 dnf5 group install -y --with-optional virtualization
+
+# remove the firefox RPM that's missing codecs
+dnf5 remove -y firefox
 
 dnf5 clean all
 
@@ -44,10 +45,13 @@ dnf5 clean all
 DOCKFMT_SHA256="f6bc025739cf4f56287e879c75c11cc73ebafdf93a57c9bcd8805d1ab82434a0"
 curl -fSL "https://github.com/jessfraz/dockfmt/releases/download/v0.3.3/dockfmt-linux-amd64" -o "/tmp/dockfmt"
 echo "${DOCKFMT_SHA256} /tmp/dockfmt" | sha256sum -c -
-install /tmp/dockfmt /usr/bin
+install /tmp/dockfmt /usr/local/bin
 
 # remove old desktop files for emacs before copying our custom file over later
 rm /usr/share/applications/emacs.desktop /usr/share/applications/emacs-mail.desktop
+
+# update doom when users log in
+systemctl --global enable doom-update.service
 
 
 ###
@@ -58,9 +62,8 @@ git clone https://github.com/ohmyzsh/ohmyzsh /usr/local/etc/ohmyzsh
 
 
 ###
-### systemd
+### misc
 ###
-
 systemctl enable libvirtd
 systemctl enable podman.socket
 # uncomment to turn on tailscale by default
--- system_files/usr/libexec/doom-update.sh
+++ system_files/usr/libexec/doom-update.sh
@@ -3,15 +3,12 @@
 set -ouex pipefail
 
 
-notify-send "Doom Emacs" "Updating Doom. Please refrain from opening Emacs for a moment"
+notify-send -a "Doom Emacs" "Updating Doom" "Please refrain from opening Emacs for a moment"
 
 # get doom emacs ready to go, regardless of whether or not it is already installed
 
-# TODO make sure this script does what it says it does
-
 TODAY=$(date +%Y%m%d)
-# OLD_BACKUPS=(ls $HOME | grep -P ".emacs.d.backup*")
-# OLD_BACKUPS+=(ls $HOME/.config | grep -P "emacs.backup*")
+OLD_BACKUPS=$(find $HOME -maxdepth 2 -regex '.*emacs[\.d]*\.backup' 2>/dev/null)
 
 # backup ~/.emacs.d if it exists
 if [ -d $HOME/.emacs.d ]; then
@@ -25,16 +22,14 @@ fi
 
 # # copy the current version of doom over to XDG home
 if [ ! -d $HOME/.config/emacs ]; then
-  rsync -r /usr/local/etc/emacs $HOME/.config/emacs
+  rsync -r /usr/local/etc/emacs $HOME/.config/
 fi
 
 $HOME/.config/emacs/bin/doom install --doomdir ~/.config/doom --force || \
-  (notify-send "Doom Emacs" "Failed to install Doom. Try running 'doom install' on your own" && exit 1)
+  (notify-send -a "Doom Emacs" "Something went wrong" "Failed to install Doom. Try running 'doom install' on your own" && exit 1)
 $HOME/.config/emacs/bin/doom sync --doomdir ~/.config/doom -e --force || \
-  (notify-send "Doom Emacs" "Failed to sync Doom. Try running 'doom sync' on your own" && exit 1)
+  (notify-send -a "Doom Emacs" "Something went wrong" "Failed to sync Doom packages. Try running 'doom sync' on your own" && exit 1)
 
-notify-send "Doom Emacs" "Ready to go\! Yay Evil\!"
+notify-send -a "Doom Emacs" "Update complete" "Ready to go\! Yay Evil\!"
 
-# TODO clean up old backups. needs testing, this is wrong
-# ls $HOME | grep -P ".emacs.d.backup-{!${TODAY}}$" | xargs -d"\n" rm
-# ls $HOME/.config | grep -P "emacs.backup-{!${TODAY}}$" | xargs -d"\n" rm
+rm -rf $OLD_BACKUPS
